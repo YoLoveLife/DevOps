354b0db75f15049b9233611fd43163e95b79b1f4
Yo <YoLoveLife@outlook.com>
2017-02-10 16:10:04 +0800

    set a urls 'test' if someone touch the /anweb/test the server will echo something to a file and we will see on the filesystem

--- anweb/views.py
+++ anweb/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from event import allevent
+from django.http import HttpResponse
+def test(request):
+    allevent.evt_shell_control('redis-server',control='echo "This is test">>/test.txt')
+    return HttpResponse('this is test result')
