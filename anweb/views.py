c54d3b9936c14b53f29e604f0abd61ffec624640
Yo <YoLoveLife@outlook.com>
2017-02-14 10:54:12 +0800

    目录结构调整 静态资源设置

--- anweb/views.py
+++ anweb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # Create your views here.
 from event import allevent
 from django.http import HttpResponse
-
-def test(request):
+from django.views.generic.detail import DetailView
+def index(request):
     #return HttpResponse('this is test result')
     return render(request, 'index.html')
