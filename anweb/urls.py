b9156709993cac65bbb8d81fde95d878052b70ae
Yo <YoLoveLife@outlook.com>
2017-02-21 17:23:07 +0800

    Group前台内容

--- anweb/urls.py
+++ anweb/urls.py
@@ -10,7 +10,9 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
    url(r'^$',views.index,name='index'),
-   url(r'^groupsearch/',views.get_group_list,name='groupsearch')
+   url(r'^cherry/group',views.cherry_group,name='cherry_group'),
+   url(r'^cherry/host', views.cherry_host, name='cherry_host'),
+  # url(r'^groupsearch/',views.get_group_list,name='groupsearch')
   # url(r'^group/$',views.group,name='group'),
   # url(r'^host/$',views.host,name='host'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
