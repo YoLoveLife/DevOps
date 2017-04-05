190bb5114ba68114a5563afbc26f6f05d8bc73ee
Yo <YoLoveLife@outlook.com>
2017-04-05 17:28:03 +0800

    文件调取 本地时间戳命名存储

--- executor/dispatch/dispatchpb.py
+++ executor/dispatch/dispatchpb.py
@@ -7,21 +7,28 @@ from util import FTP
 from modules.persontask import PersonTask
 from modules.personbook import PersonBook
 from modules.personblock import PersonBlock
+import time,os
 from executor.dispatch import REDIS_CONF,MYSQL_CONF,NGINX_CONF,TOMCAT_CONF
-
-def dispatch_getcnf(taskname,cnffile):
-    _ext_vars={'filename':taskname,
-               'cnffile':cnffile,
+FILENAME=r'/tmp/%s'
+def dispatch_getcnf(cnffile):
+    t=time.time()
+    _ext_vars={'cnffile':cnffile,
+               'filename':t,
                }
     personblock=PersonBlock()
     personblock.add_extendvars(_ext_vars)
     pb=PersonBook("dispatch getcnf",'no')
     task1=PersonTask(module="fetch",args="src={{cnffile}} dest=/tmp/{{filename}} flat=yes",)
+    #task2=PersonTask(module="shell",args="find ./ -name 'hosts.*.[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\@[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\~' -mmin +60 -exec rm -f {} \;")
     pb.add_task(task1)
     personblock.set_playbook(pb)
     personblock.run_block()
+    f = open(FILENAME%t)
+    try:
+        str=f.read()
+    finally:
+        f.close()
+        os.remove(FILENAME%t)
+    return str
 
-dispatch_getcnf('redis-server','/etc/hosts')
-f=open(r'/tmp/redis-server')
-print(f.read())
-f.close()
+print(dispatch_getcnf('/etc/hosts'))
--- scripts/tomcat/tomcat_test.sh
+++ scripts/tomcat/tomcat_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author Yo
 # Email YoLoveLife@outlook.com
-# Time 2017-03-07 08:56
-ONLINE=`ps aux |grep redis-server|wc -l`
+# Time 2017-04-05 16:19
+ONLINE=`ps aux |grep java |wc -l`
 if [ "${ONLINE}" == "2" ];then
     echo "1"
 else
