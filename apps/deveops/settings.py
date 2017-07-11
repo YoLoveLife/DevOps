a040e1f344eff52a94fdd3845ba97697bc1682d3
Yo <YoLoveLife@outlook.com>
2017-07-11 14:43:43 +0800

    数据库修改

--- apps/deveops/settings.py
+++ apps/deveops/settings.py
@@ -35,14 +35,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'validate',
     'manager',
+    'validate',
 ]
 
 MIDDLEWARE = [
