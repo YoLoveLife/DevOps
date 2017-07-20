b4089f7a487b143be674ed0efb9bc8376e290120
YoLoveLife <YoLoveLife@outlook.com>
2017-07-20 10:43:04 +0800

    serializers create rewrite

--- apps/manager/serializers.py
+++ apps/manager/serializers.py
@@ -1,19 +1,26 @@
 from .models import Host,Group
 from rest_framework import serializers
-from rest_framework.viewsets import ModelViewSet
-from django.contrib.auth.models import User
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('id','name','remark')
-
-class HostSerializer(serializers.HyperlinkedModelSerializer):
+class HostSerializer(serializers.ModelSerializer):
     group_id=serializers.CharField(source="group.id")
     class Meta:
         model=Host
         fields = ('id','name','group_id','sship','sshpasswd','sshport')
         depth = 1
+    def create(self, validated_data):
+        print(validated_data)
+        group=Group.objects.get(id=validated_data['group']['id'])
+        host=Host(
+            name=validated_data['name'],
+            group=group,
+            sship=validated_data['sship'],
+            sshpasswd=validated_data['sshpasswd'],
+            sshport=validated_data['sshport'],
+        )
+        return host
 
-
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'name', 'remark')
 
