84970f8700feee099982beecbafd6680fa537dd7
YoLoveLife <YoLoveLife@outlook.com>
2017-07-28 14:42:01 +0800

    需要解决form表单的更新问题

--- apps/manager/models.py
+++ apps/manager/models.py
@@ -7,8 +7,8 @@ from django.db import models
 from django.db import models
 class Group(models.Model):
     id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=100)
-    info=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,default='None')
+    info=models.CharField(max_length=100,default='None')
 
 class Storage(models.Model):
     id=models.AutoField(primary_key=True)#全局ID
