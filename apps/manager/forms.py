b839700d0de50eb3eb10ff451357fdd4635a3752
YoLoveLife <YoLoveLife@outlook.com>
2017-08-02 17:47:46 +0800

    django to page{{}}

--- apps/manager/forms.py
+++ apps/manager/forms.py
@@ -28,26 +28,32 @@ class GroupForm(forms.Form):
             return {'success':True,'msg':'','data':self.data}
 
 
-class HostForm(forms.Form):
-    id = forms.IntegerField(label="ID",
-                            widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'readonly': 'yes'}))
-    c = models.Group.objects.all().values_list('id','name')
-    group=forms.CharField(label="GroupSelect",widget=forms.Select(choices=c,attrs={'class':'form-control'}))
-    storage=forms.CharField(label="Storage",max_length=100)
-    storagesss=forms.ModelMultipleChoiceField(label="Storagesss",widget=forms.CheckboxSelectMultiple,queryset=models.Storage.objects)
-    systemtype=forms.CharField(label="SystemType",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    manage_ip=forms.CharField(label="ManagerID",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    service_ip=forms.CharField(label="ServiceIP",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    outer_ip=forms.CharField(label="OuterIP",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    server_position=forms.CharField(label="ServerPos",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    hostname=forms.CharField(label="Hostname",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    normal_user=forms.CharField(label="NormalUser",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    sshpasswd=forms.CharField(label="Passwd",max_length=100,widget=forms.TextInput(attrs={'type': 'password', 'class': 'form-control'}))
-    sshport=forms.CharField(label="Port",max_length=5,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    coreness=forms.CharField(label="Coreness",max_length=5,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    memory=forms.CharField(label="Memory",max_length=7,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    root_disk=forms.CharField(label="RootDisk",max_length=7,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
-    info=forms.CharField(label="Info",max_length=200,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+class HostForm(forms.ModelForm):
+    class Meta:
+        model = models.Host
+        fields = ('id','group','storages','systemtype',
+                 'manage_ip','service_ip','outer_ip',
+                 'server_position','hostname','normal_user',
+                 'sshpasswd','sshport','coreness','memory','root_disk','info')
+    # id = forms.IntegerField(label="ID",
+    #                         widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'readonly': 'yes'}))
+    # c = models.Group.objects.all().values_list('id','name')
+    # group=forms.CharField(label="GroupSelect",widget=forms.Select(choices=c,attrs={'class':'form-control'}))
+    # storage=forms.CharField(label="Storage",max_length=100)
+    # storagesss=forms.ModelMultipleChoiceField(label="Storagesss",widget=forms.CheckboxSelectMultiple,queryset=models.Storage.objects)
+    # systemtype=forms.CharField(label="SystemType",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # manage_ip=forms.CharField(label="ManagerID",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # service_ip=forms.CharField(label="ServiceIP",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # outer_ip=forms.CharField(label="OuterIP",max_length=15,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # server_position=forms.CharField(label="ServerPos",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # hostname=forms.CharField(label="Hostname",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # normal_user=forms.CharField(label="NormalUser",max_length=50,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # sshpasswd=forms.CharField(label="Passwd",max_length=100,widget=forms.TextInput(attrs={'type': 'password', 'class': 'form-control'}))
+    # sshport=forms.CharField(label="Port",max_length=5,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # coreness=forms.CharField(label="Coreness",max_length=5,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # memory=forms.CharField(label="Memory",max_length=7,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # root_disk=forms.CharField(label="RootDisk",max_length=7,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    # info=forms.CharField(label="Info",max_length=200,widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
 
     def is_valid(self):
         if self.data['manage_ip']!="" and self.data.has_key('group'):
