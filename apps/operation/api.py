4e55d04433d3d230ddf0229f96cba250a1bfe902
YoLoveLife <YoLoveLife@outlook.com>
2017-08-17 16:34:04 +0800

    完成scriptargs所有API

--- apps/operation/api.py
+++ apps/operation/api.py
@@ -1,38 +1,46 @@
 # -*- coding:utf-8 -*-
-from .models import Script,ScriptArgs
+import models
 import serializers
 from rest_framework.views import Response,status
 from rest_framework import generics
 from forms import ScriptForm
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 class ScriptListAPI(generics.ListAPIView):
-    module = Script
     serializer_class = serializers.ScriptSerializer
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
-        queryset=Script.objects.all()
+        queryset=models.Script.objects.all()
         return queryset
 
 class ScriptArgsListAPI(generics.ListAPIView):
-    module = ScriptArgs
     serializer_class = serializers.ScriptArgsSerializer
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         if self.kwargs['pk']=='0':
             queryset={}
             return queryset
-        queryset = Script.objects.get(id=self.kwargs['pk']).scriptargs
+        queryset = models.Script.objects.get(id=self.kwargs['pk']).scriptargs
         return queryset
 
+class ScriptArgsCreateAPI(generics.CreateAPIView):
+    serializer_class = serializers.ScriptArgsSerializer
+    permission_classes = [AllowAny]
 
-class ScriptUpdateArgsAPI(generics.UpdateAPIView):
-    module = Script
-    serializer_class = serializers.ScriptSerializer
-    permission_classes = [IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        script = models.Script.objects.get(id=int(kwargs['pk']))
+        serializer.instance.script=script
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def get_queryset(self):
-        print('a')
-        return
-    def patch(self, request, *args, **kwargs):
-        print('a')
-        return
+class ScriptRemoveArgsAPI(generics.DestroyAPIView):
+        serializer_class = serializers.ScriptArgsSerializer
+        permission_classes = [AllowAny]
+        queryset = models.ScriptArgs.objects.all()
+
+        def delete(self, request, *args, **kwargs):
+            return self.destroy(request, *args, **kwargs)
+
