e75d9c58c36ecc573eb2b042e1eaf36c032520ab
YoLoveLife <YoLoveLife@outlook.com>
2017-09-18 11:26:03 +0800

    添加travis测试

--- apps/manager/models.py
+++ apps/manager/models.py
@@ -37,7 +37,7 @@ class Host(models.Model):
     groups = models.ManyToManyField(Group,blank=True,related_name='hosts',verbose_name=_("Group"))#所属应用
     storages = models.ManyToManyField(Storage,blank=True,related_name='hosts',verbose_name=_('Host'))
     systemtype=models.IntegerField(default=0,choices=SYSTEM_CHOICES)#操作系统
-    manage_ip = models.CharField(max_length=15, default='',null=True)#管理IP
+    manage_ip = models.CharField(max_length=15, default='192.168.0.0',null=True)#管理IP
     service_ip = models.CharField(max_length=15, default='')#服务IP
     outer_ip = models.CharField(max_length=15, default='',null=True)#外网IP
     server_position = models.CharField(max_length=50,default='')#服务器位置
--- apps/validate/tests.py
+++ apps/validate/tests.py
@@ -1,3 +1,19 @@
+# -*- coding:utf-8 -*-
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import Group
+import models
+class ExtendUserTestCase(TestCase):
+    def setUp(self):
+        grp=Group.objects.get(name='普通用户')
+        models.ExtendUser.objects.create(username='testuser',first_name='test',last_name='name',group=grp)
+
+    def test_user_getfullname(self):
+        testuser = models.ExtendUser.objects.get(username='testuser')
+        self.assertEqual(testuser.get_full_name(),'testname')
+
+    def test_user_getgroupname(self):
+        testuser = models.ExtendUser.objects.get(username='testuser')
+        self.assertEqual(testuser.get_group_name(),'普通用户')
