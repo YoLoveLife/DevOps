6d22e21368b3a64ddcb753e6ed348e62990d7043
YoLoveLife <YoLoveLife@outlook.com>
2017-10-16 22:04:50 +0800

    完成数据库数据获取存储

--- apps/execute/callback/catch/basic.py
+++ apps/execute/callback/catch/basic.py
@@ -5,10 +5,10 @@ class BasicResultCallback(ResultCallback):
         super(BasicResultCallback,self).__init__()
 
     def v2_runner_on_ok(self, result, **kwargs):
-        super(BasicResultCallback,self).v2_runner_on_ok(result._result['msg'],**kwargs)
+        super(BasicResultCallback,self).v2_runner_on_ok(result,**kwargs)
 
     def v2_runner_on_unreachable(self, result):
-        super(BasicResultCallback,self).v2_runner_on_unreachable()
+        super(BasicResultCallback,self).v2_runner_on_unreachable(result)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
         super(BasicResultCallback,self).v2_runner_on_failed(result,ignore_errors)
