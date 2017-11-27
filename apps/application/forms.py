f9cdcad114cb248234700ae4903aa98a7dcf70a5
YoLoveLife <YoLoveLife@outlook.com>
2017-11-27 11:14:46 +0800

    new-api Adh

--- apps/application/forms.py
+++ apps/application/forms.py
@@ -6,6 +6,8 @@
 from django import forms
 import models
 from django.utils.translation import gettext_lazy as _
+from manager.models import Host
+from deveops.utils import aes,checkpass
 #__all__ = ['DBCreateUpdateForm']
 class DBCreateUpdateForm(forms.ModelForm):
     service_ip = forms.CharField(required=True,max_length=13,label='业务IP')
@@ -20,3 +22,24 @@ class DBCreateUpdateForm(forms.ModelForm):
             'prefix':'prefix','root_passwd':'管理密码','port':'服务端口',
             'socket':'Socket','datadir':'数据目录',
         }
+
+    def clean_service_ip(self):
+        if Host.objects.filter(service_ip=self.cleaned_data['service_ip']).exists():
+            pass
+        else:
+            raise forms.ValidationError(u'该主机不存在')
+        service_ip = self.cleaned_data['service_ip']
+        host = Host.objects.filter(service_ip=service_ip).get()
+        self.instance.host = host
+        return host
+
+    def clean_root_passwd(self):
+        root_passwd = self.cleaned_data['root_passwd']
+        if checkpass.checkPassword(root_passwd):
+            self.instance.root_passwd = aes.encrypt(root_passwd)
+            return aes.encrypt(root_passwd)
+        else:
+            raise forms.ValidationError(u'密码复杂度不足')
+
+    def before_save(self,request):
+        pass
---
+++ apps/deveops/utils/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python
+# Time 17-11-21
+# Author Yo
+# Email YoLoveLife@outlook.com
