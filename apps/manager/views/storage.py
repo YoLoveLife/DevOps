a1ddb5fb2b72f10ca53af338c488dcf81cd6f29d
YoLoveLife <YoLoveLife@outlook.com>
2017-12-25 17:27:15 +0800

    rebuild manager

--- apps/manager/views/storage.py
+++ apps/manager/views/storage.py
@@ -16,43 +16,39 @@ class ManagerStorageListView(LoginRequiredMixin,TemplateView):
         return super(ManagerStorageListView,self).get(request,*args,**kwargs)
 
 class ManagerStorageCreateView(LoginRequiredMixin,StoragePermission.StorageAddRequiredMixin,CreateView):
-    template_name = 'manager/new_update_storage.html'
+    template_name = 'manager/new_storage.html'
     form_class = forms.StorageCreateUpdateForm
     success_url = reverse_lazy('manager:storage')
     model = models.Storage
 
     @decorator_manager(0,u'新增存储')
     def form_valid(self, form):
-        # form.before_save(request=self.request,commit=True)
+        form.save_hosts_new()
         return self.request.user,super(ManagerStorageCreateView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(ManagerStorageCreateView, self).get_context_data(**kwargs)
-        # hosts = models.Host.objects.all()
-        # context.update({
-        #     'hosts':hosts,'storage_hosts':{}
-        # })
         return context
 
     def get_success_url(self):
         return self.success_url
 
 class ManagerStorageUpdateView(LoginRequiredMixin,StoragePermission.StorageChangeRequiredMixin,UpdateView):
-    template_name = 'manager/new_update_storage.html'
+    template_name = 'manager/update_storage.html'
     form_class = forms.StorageCreateUpdateForm
     success_url = reverse_lazy('manager:storage')
     model = models.Storage
 
     @decorator_manager(0,u'更新存储')
     def form_valid(self, form):
-        # form.before_save(request=self.request,commit=True)
+        form.save_hosts_update()
         return self.request.user,super(ManagerStorageUpdateView,self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(ManagerStorageUpdateView,self).get_context_data(**kwargs)
-        # hosts = models.Host.objects.all()
-        # storage_hosts=[host.id for host in self.object.hosts.all()]
-        # context.update({
-        #     'hosts':hosts,'storage_hosts':storage_hosts
-        # })
+        hosts = models.Host.objects.all()
+        storage_hosts=[host.id for host in self.object.hosts.all()]
+        context.update({
+            'hosts':hosts,'storage_hosts':storage_hosts
+        })
         return context
