e811ac38ce3c33e31ed4e9e63df37fdd235ccf23
YoLoveLife <YoLoveLife@outlook.com>
2018-01-12 17:42:51 +0800

    重新书写数据库配置 通过线上测试

--- apps/deveops/settings.py
+++ apps/deveops/settings.py
@@ -108,7 +108,7 @@ DATABASES={
         'ENGINE':'django.db.backends.mysql',
         'NAME':'deveops',
         'USER':'root',
-        'PASSWORD':'daiSgmiku2',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     },
