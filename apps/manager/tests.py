043f5fdf38ce95aa5f3affa94dfea15c262cde1d
YoLoveLife <YoLoveLife@outlook.com>
2018-01-19 22:22:36 +0800

    测试用例修改 && readme change

--- apps/manager/tests.py
+++ apps/manager/tests.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 from django.test import TestCase
-from manager.models import Group,Host,Storage
+from manager.models import Group,Host,Storage,System_Type
 #model test
 class GroupTestCase(TestCase):
     def setUp(self):
@@ -38,13 +38,32 @@ class StorageTestCase(TestCase):
         Storage.objects.filter(disk_size='1024GB').delete()
         self.assertEqual(Storage.objects.filter(disk_size='1024GB').count(),0)
 
+class System_TypeTestCase(TestCase):
+    def setUp(self):
+        System_Type.objects.create(name='CentOS6.5')
+
+    def test_systemtypecreate(self):
+        systemtype = System_Type.objects.get(name='CentOS6.5')
+        self.assertEqual(systemtype.name,'CentOS6.5')
+
+    def test_systemtypeupdate(self):
+        System_Type.objects.filter(name='CentOS6.5').update(name='CentOS6.6')
+        systemtype = System_Type.objects.get(name='CentOS6.6')
+        self.assertEqual(systemtype.name,'CentOS6.6')
+
+    def test_systemtypedelete(self):
+        System_Type.objects.filter(id=1).delete()
+        self.assertEqual(System_Type.objects.filter(id=1).count(),0)
+
 class HostTestCase(TestCase):
     def setUp(self):
         Group.objects.create(name='Testgroup',info='Testgroupinfo')
         Storage.objects.create(disk_size='1024GB', disk_path='//192.168.0.1/testdisk', info='testdisk')
+        System_Type.objects.create(name='CentOS6.5')
         groups = Group.objects.filter(name='Testgroup')
         storages = Storage.objects.filter(disk_size='1024GB')
-        host = Host.objects.create(systemtype=1,
+        systemtype = System_Type.objects.filter(name='CentOS6.5').get()
+        host = Host.objects.create(
                             manage_ip='192.168.1.1',service_ip='192.168.1.2',
                             outer_ip='192.168.1.3',server_position='VmWare',
                             hostname='localhost.localdomain',normal_user='TestProd',
@@ -52,6 +71,7 @@ class HostTestCase(TestCase):
                             memory='1024MB',root_disk='1024G',info='testinfo')
         host.storages = storages
         host.groups = groups
+        host.systemtype = systemtype
         host.save()
 
     def test_hostcreate(self):
