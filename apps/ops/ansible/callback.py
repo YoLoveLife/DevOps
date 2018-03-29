71553619db89284057fbe8666e57c66c0c718862
余先生 <yolovelife@outlook.com>
2018-03-29 19:52:13 +0800

    完成可执行的基于ansible的元操作

---
+++ apps/ops/ansible/callback.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python
+# Time 18-3-29
+# Author Yo
+# Email YoLoveLife@outlook.com
+from ansible.plugins.callback import CallbackBase
+INDENT = 4
+
+
+class AnsibleCallback(CallbackBase):
+
+    def __init__(self, replay_name):
+        self.replay_name = replay_name
+        return super(AnsibleCallback, self).__init__()
+
+    def v2_runner_on_ok(self, result, **kwargs):
+        from deveops.asgi import channel_layer
+        if not result._task.action=='setup':
+            message = "{host} => SUCCESS\r\n".format(host=result._host.get_name())
+            channel_layer.send(self.replay_name,
+                               {'text': message})
+            channel_layer.send(self.replay_name, {'text':self._dump_results(result._result,indent=INDENT).replace('\n','\r\n')+'\r\n'})
+
+
+    def v2_runner_on_unreachable(self, result):
+        from deveops.asgi import channel_layer
+        message = "{host} => UNREACHABLE!\r\n".format(host=result._host.get_name())
+        channel_layer.send(self.replay_name, {'text': message})
+        channel_layer.send(self.replay_name,
+                           {'text': self._dump_results(result._result, indent=INDENT).replace('\n', '\r\n') + '\r\n'})
+
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        from deveops.asgi import channel_layer
+        message = "{host} => FAILED!\r\n".format(host=result._host.get_name())
+
+        channel_layer.send(self.replay_name, {'text': message})
+        channel_layer.send(self.replay_name, {'text': self._dump_results(result._result, indent=INDENT)})
--- apps/ops/api/meta.py
+++ apps/ops/api/meta.py
@@ -11,13 +11,14 @@ from deveops.api import WebTokenAuthentication
 class OpsMetaListAPI(WebTokenAuthentication,generics.ListAPIView):
     module = models.META
     serializer_class = serializers.MetaSerializer
-    permission_classes = [MetaPermission.MetaListRequiredMixin,IsAuthenticated]
-    # permission_classes = [AllowAny]
+    # permission_classes = [MetaPermission.MetaListRequiredMixin,IsAuthenticated]
+    permission_classes = [AllowAny]
 
     # 所有運維工程師有如下特點
     # 1、僅能查看自己所管理的應用組
     # 2、可以增删改自己所管理的应用组的所有Meta操作
     def get_queryset(self):
-        queryset = models.META.objects.filter(group__users__id=self.request.user.id)
+        # queryset = models.META.objects.filter(group__users__id=self.request.user.id)
+        queryset = models.META.objects.all()
         return queryset
 
