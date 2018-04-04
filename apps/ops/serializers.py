aded73a7a9c9f6029eed4c28ee4f3cccf9f8dbb8
余先生 <yolovelife@outlook.com>
2018-04-04 18:35:36 +0800

    更新部分API

--- apps/ops/serializers.py
+++ apps/ops/serializers.py
@@ -5,12 +5,25 @@ __all__ = [
     "MetaSerializer"
 ]
 
+
+class MetaContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.META_CONTENT
+        fields = (
+            'id', 'name', 'module', 'args', 'sort'
+        )
+        read_only_fields = (
+            'id',
+        )
+
+
 class MetaSerializer(serializers.ModelSerializer):
     hosts = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Host.objects.all())
-    contents = serializers.PrimaryKeyRelatedField(many=True, queryset=models.META_CONTENT.objects.all())
+    contents = MetaContentSerializer(required=True, many=True, allow_null=True)
     group = serializers.PrimaryKeyRelatedField(queryset=models.Group.objects.all())
     group_name = serializers.CharField(source="group.name")
     user_list = serializers.ListField(source="group.users_list_byhostname")
+
     class Meta:
         model = models.META
         fields = (
